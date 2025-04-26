a980bdef0bd5090fd98b46e20e9c1d508c94f58a
Mete Gulsoy <mgulsoy@etu.edu.tr>
2025-04-26 20:01:38 +0300

    asd

--- backend.py
+++ backend.py
@@ -1,24 +1,17 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import FileResponse
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, request, send_file
+from flask_cors import CORS
 import BackendModelExecutor
 import os
 from werkzeug.utils import secure_filename
-app = FastAPI()
 
-# Enable CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # You can specify specific origins here
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = Flask(__name__)
+CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = "outputs"
-app.state.UPLOAD_FOLDER = UPLOAD_FOLDER
-app.state.OUTPUT_FOLDER = OUTPUT_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -26,32 +19,32 @@ if not os.path.exists(UPLOAD_FOLDER):
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
 
-@app.post('/upload-and-run-model')
-async def upload_files(
-    video: UploadFile = File(...),
-    model_file: UploadFile = File(None),
-    model_name: str = Form(None)
-):
+@app.route('/upload-and-run-model', methods=['POST'])
+def upload_files():
+    video = request.files.get('video')
+    model_file = request.files.get('model')
+    model_name = request.form.get('model_name')
+
     video_filename = secure_filename(video.filename)
-    video_path = os.path.join(app.state.UPLOAD_FOLDER, video_filename)
-    output_path = os.path.join(app.state.OUTPUT_FOLDER, video_filename)
+    video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_filename)
+    output_path = os.path.join(OUTPUT_FOLDER, video_filename)
 
     # Save video
-    with open(video_path, "wb") as f:
-        f.write(await video.read())
-
-    model_path = None
+    video.save(video_path)
+    
     if model_file:
         model_filename = secure_filename(model_file.filename)
-        model_path = os.path.join(app.state.UPLOAD_FOLDER, model_filename)
-        with open(model_path, "wb") as f:
-            f.write(await model_file.read())
+        model_path = os.path.join(app.config['UPLOAD_FOLDER'], model_filename)
+        model_file.save(model_path)
     elif model_name:
         model_path = model_name
 
-    # Execute the model
     modelExecutor = BackendModelExecutor.ModelExecutor(model_path, video_path, output_path)
     print("Executing Model")
     modelExecutor.executeModel()
 
-    return FileResponse(output_path, headers={"Content-Disposition": f"attachment; filename={video_filename}"})
+    return send_file(output_path, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
